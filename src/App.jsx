7845f563e863b1367baf87be08d5187783cb5330
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-05-07 22:53:13 +0500

    update files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
+import HeaderInput from './Pages/HeaderInput/HeaderInput';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <HeaderInput />
     </div>
   );
 }
