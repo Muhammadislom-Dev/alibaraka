cec2b828152c39b4ba7f720a541f70cf1abb1ddc
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-05-08 22:49:19 +0500

    update files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import './App.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from 'react';
 import HeaderInput from './Pages/HeaderInput/HeaderInput';
 
 function App() {
+
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
   return (
     <div className="App">
       <HeaderInput />
