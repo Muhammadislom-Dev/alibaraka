9caf3d710fd3a64b9a700374a19833940f63f9c6
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-05-10 21:19:12 +0500

    Initial commit

---
+++ src/components/Accordion/Accordion.jsx
@@ -0,0 +1,51 @@
+import './Accordion.css'
+
+import * as React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+const Accordions =()=>{
+return(
+<div className="accordion">
+    <div className="container">
+        <Accordion>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+            >
+            <Typography>Accordion 1</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                    malesuada lacus ex, sit amet blandit leo lobortis eget.
+                </Typography>
+            </AccordionDetails>
+        </Accordion>
+        <Accordion>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2a-content"
+            id="panel2a-header"
+            >
+            <Typography>Accordion 2</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                    malesuada lacus ex, sit amet blandit leo lobortis eget.
+                </Typography>
+            </AccordionDetails>
+        </Accordion>
+        <Accordion disabled>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel3a-content" id="panel3a-header">
+            </AccordionSummary>
+        </Accordion>
+    </div>
+</div>
+)
+}
+export default Accordions
